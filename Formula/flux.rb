bde333dec58033c663c5acfb9af6f425fe2e4bd0
goreleaserbot <goreleaser@carlosbecker.com>
2021-06-02 20:09:11 +0200

    Brew formula update for flux version v0.14.2

--- Formula/flux.rb
+++ Formula/flux.rb
@@ -5,24 +5,29 @@
 class Flux < Formula
   desc "Flux CLI"
   homepage "https://fluxcd.io/"
-  version "0.14.1"
+  version "0.14.2"
   bottle :unneeded
 
-  if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/fluxcd/flux2/releases/download/v0.14.1/flux_0.14.1_darwin_amd64.tar.gz"
-    sha256 "99841bf12826a70e557fc84435cfbde2120f53bbce4deb847999e231011573d4"
+  on_macos do
+    if Hardware::CPU.intel?
+      url "https://github.com/fluxcd/flux2/releases/download/v0.14.2/flux_0.14.2_darwin_amd64.tar.gz"
+      sha256 "a3e1142a7cbffa880e2dc290eab8f67a745ddab8d686ad27746f99b1185e4147"
+    end
+    if Hardware::CPU.arm?
+      url "https://github.com/fluxcd/flux2/releases/download/v0.14.2/flux_0.14.2_darwin_arm64.tar.gz"
+      sha256 "1642507fdc37b934eecc7952a16d6c6f65122c8920b1b33a5994120b7d1b0920"
+    end
   end
-  if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/fluxcd/flux2/releases/download/v0.14.1/flux_0.14.1_darwin_arm64.tar.gz"
-    sha256 "0da8dde541ea011a5f32059b93354d3d9720b7a67340d4ef40102f136c60543b"
-  end
-  if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/fluxcd/flux2/releases/download/v0.14.1/flux_0.14.1_linux_amd64.tar.gz"
-    sha256 "0f570486c946dea7e4c3c5063b84e6673b470fb113567116cb61e55a94d8aaeb"
-  end
-  if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/fluxcd/flux2/releases/download/v0.14.1/flux_0.14.1_linux_arm64.tar.gz"
-    sha256 "cdb49dfa18fb9c0a503f72352c3b08177610a9b17ea488a67c3f0d1a28254d7d"
+
+  on_linux do
+    if Hardware::CPU.intel?
+      url "https://github.com/fluxcd/flux2/releases/download/v0.14.2/flux_0.14.2_linux_amd64.tar.gz"
+      sha256 "a3ccc7fbca20bb9a5ab1f07ffd7c0ba588f349515253cd53b3525bb73ab3db2a"
+    end
+    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+      url "https://github.com/fluxcd/flux2/releases/download/v0.14.2/flux_0.14.2_linux_arm64.tar.gz"
+      sha256 "66882bf48b891c904d1840daf35e989e8b3febc40f7038bedc370a05ef308929"
+    end
   end
 
   depends_on "kubectl" => :optional
