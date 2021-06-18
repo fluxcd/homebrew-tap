5457c544afcff62542975248a363206a844e03aa
goreleaserbot <goreleaser@carlosbecker.com>
2021-06-18 17:13:26 +0200

    Brew formula update for flux version v0.15.2

--- Formula/flux.rb
+++ Formula/flux.rb
@@ -5,28 +5,28 @@
 class Flux < Formula
   desc "Flux CLI"
   homepage "https://fluxcd.io/"
-  version "0.15.1"
+  version "0.15.2"
   bottle :unneeded
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/fluxcd/flux2/releases/download/v0.15.1/flux_0.15.1_darwin_amd64.tar.gz"
-      sha256 "36827c90f5f86bb7ee050edf8253f8b49c106ca4f7a08f9dba2a6a15797afa37"
+      url "https://github.com/fluxcd/flux2/releases/download/v0.15.2/flux_0.15.2_darwin_amd64.tar.gz"
+      sha256 "6fa5ba372b81488a6bb42cb5d8c0926ec0216710821b528d201e68b240d8e82d"
     end
     if Hardware::CPU.arm?
-      url "https://github.com/fluxcd/flux2/releases/download/v0.15.1/flux_0.15.1_darwin_arm64.tar.gz"
-      sha256 "b4b1833e8d6d1a37161faac5c775b5ecd6d1b33507abe13a0d7a0293f02305d4"
+      url "https://github.com/fluxcd/flux2/releases/download/v0.15.2/flux_0.15.2_darwin_arm64.tar.gz"
+      sha256 "1b89cd43aae3dd82f5162e72b33c080a7825e02cf87f120f4161091d3a2208f0"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/fluxcd/flux2/releases/download/v0.15.1/flux_0.15.1_linux_amd64.tar.gz"
-      sha256 "1924ab72c59dd874fcc0fb772b31b841411fa1f7d3837290f097c8f2c1228e33"
+      url "https://github.com/fluxcd/flux2/releases/download/v0.15.2/flux_0.15.2_linux_amd64.tar.gz"
+      sha256 "b13d801005cea27fb8255a8323d72505bb0b59265622c680c112d48d26a7fcec"
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/fluxcd/flux2/releases/download/v0.15.1/flux_0.15.1_linux_arm64.tar.gz"
-      sha256 "06f7df89bde714a9c1696215925485c7cfa1931d0906df4a789f1aa5c986048f"
+      url "https://github.com/fluxcd/flux2/releases/download/v0.15.2/flux_0.15.2_linux_arm64.tar.gz"
+      sha256 "80c74d4d735f56cad4716aee7d21fe7787a2696b35e5a6e6fe8b5e0d1220f901"
     end
   end
 
